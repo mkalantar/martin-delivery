53530d3612b2438b781d859ccae3519df284bcb7
Morteza Kalantar <m.kalantar@dsi.ir>
2023-01-28 16:19:53 +0330

    customer endpoints are done

--- app/Http/Controllers/Customer.php
+++ app/Http/Controllers/Customer.php
@@ -15,36 +15,45 @@ class Customer extends Controller
     $this->middleware(CustomerMiddleware::class);
   }
 
-  private function validateRequestValues($value) {
+  private function validateRequestValues($value)
+  {
     $keys = ['lan', 'lon', 'name', 'address', 'phone'];
-    foreach($keys as $key) {
+    foreach ($keys as $key) {
       if (!(array_key_exists($key, $value) && $value[$key])) {
         return false;
       }
     }
     return true;
   }
-  
-  public function createRequest(Request $request) {
+
+  public function createRequest(Request $request)
+  {
     if ($request->has(['from', 'to'])) {
       if ($this->validateRequestValues($request->input('from')) && $this->validateRequestValues($request->input('to'))) {
-        $lock = Cache::lock('requests', 0.5);
+        $lock = Cache::lock('requests', 1);
         $requests_list = [];
         try {
-          $lock->block(1);
+
+          $lock->block(2);
+
           if (Cache::has('requests')) {
             $requests_list = Cache::get('requests');
           }
-          $requests_list[] = $request->input();
+
+          $new_request = $request->input();
+          $new_request['isProcessing'] = false;
+          $requests_list[] = $new_request;
+
           Cache::put('requests', $requests_list);
+
           $lock->release();
 
           $id = sprintf("R-%d-%s", time(), substr(str_shuffle(md5(microtime())), 0, 7));
           Cache::put($id, count($requests_list) - 1);
 
           return response($id, 200);
+        } catch (LockTimeoutException) {
 
-        } catch(LockTimeoutException $e) {
           $lock->release();
           return response('Race condition!', 500);
         }
@@ -53,26 +62,49 @@ class Customer extends Controller
     return response('Request params are malformed', 400);
   }
 
-  public function cancelRequest(Request $request, $id) {
+  public function cancelRequest(Request $request, $id)
+  {
 
     if (!$id) {
       return response('', 400);
     }
     $index = Cache::get($id);
-    if ($index !== null) {
+    if ($index === null) {
       return response('', 404);
     }
 
     $requests_list = Cache::get('requests');
+
+    if (!array_key_exists($index, $requests_list)) {
+      return response('', 404);
+    }
+
     $req = $requests_list[$index];
     if (!$req) {
       return response('', 404);
     }
 
+    if ($req['customer'] != $request->input('customer')) {
+      return response('', 403);
+    }
+
     if ($req['isProcessing']) {
       return response('Request is on process', 423);
     }
+    Cache::forget($id);
+    $lock = Cache::lock('request', 1);
+    try {
 
+      $lock->block(2);
+      $requests_list = Cache::get('requests');
+      unset($requests_list[$index]);
+      Cache::put('requests', $requests_list);
+      $lock->release();
+    } catch (LockTimeoutException) {
+
+      $lock->release();
+      return response('Race condition!', 500);
+    }
     return response('', 200);
   }
 }
--- app/Http/Middleware/Customer.php
+++ app/Http/Middleware/Customer.php
@@ -10,27 +10,27 @@ use UnexpectedValueException;
 
 class Customer
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-     */
-    public function handle(Request $request, Closure $next)
-    {
-        $header = $request->header('Authorization');
-        $token = [];
-        if (!preg_match('/^Bearer\s(.*)/', $header, $token)) {
-          return response('', 401);
-        } else {
-          try {
-            $decoded_token = (array) JWT::decode($token[1], new Key(cache('secret'), 'HS256'));
-            if (!preg_match('/^Customer.*/', $decoded_token['aud'])) return response('', 403);
-          } catch (UnexpectedValueException $e) {
-            return response('', 401);
-          }
-        }
-        return $next($request);
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+   * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+   */
+  public function handle(Request $request, Closure $next)
+  {
+    $header = $request->header('Authorization');
+    $token = [];
+    if (!preg_match('/^Bearer\s(.*)/', $header, $token)) {
+      return response('', 401);
+    } else {
+      try {
+        $decoded_token = (array) JWT::decode($token[1], new Key(cache('secret'), 'HS256'));
+        if (!preg_match('/^Customer.*/', $decoded_token['aud'])) return response('', 403);
+      } catch (UnexpectedValueException $e) {
+        return response('', 401);
+      }
     }
+    return $next($request->merge(['customer' => $decoded_token['aud']]));
+  }
 }
