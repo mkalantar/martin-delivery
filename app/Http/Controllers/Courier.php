d5462e3fe641dfc7790d8c060fe22d9582358730
Morteza Kalantar <m.kalantar@dsi.ir>
2023-01-28 03:51:08 +0330

    add Courier controller

---
+++ app/Http/Controllers/Courier.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class Courier extends Controller
+{
+    //
+    public function getRequests() {
+      return [];
+    }
+
+    public function acceptRequest($id) {
+
+    }
+
+    public function startDelivery($id) {
+
+    }
+
+    public function finishDelivery($id) {
+      
+    }
+}
